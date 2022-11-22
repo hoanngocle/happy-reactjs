0a2fa0ed1b79497efee9d2c0c273d8c93171f92b
hoanln <hoanln@deha-soft.com>
2022-11-22 10:24:44 +0700

    Render element in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = 'Emilia';
+  const age = 28;
+  const isMale = true;
+  const student = {
+    name: "Hayd"
+  };
+
+  const colorList = [
+    'red',
+    'blue',
+    'green'
+  ];
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Emilia!
+          Hello {name}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Xin chao {name} - {age} - {isMale ? 'Male': 'Female'}</p>
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && (
+          <React.Fragment>
+          <p>Male</p>
+          <p>Male</p>
+          <p>Male</p>
+          </React.Fragment>
+        )}
+
+
+        {isMale && (
+          <>
+          <p>Male</p>
+          <p>Male</p>
+          <p>Male</p>
+          </>
+        )}
+
+        <p>{student.name}      </p>
+
+        <p>{colorList}</p>
+        <ul>
+          {colorList.map(color => (
+            <li style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
