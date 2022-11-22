3ab3f8f52c725c59f07bdc3e92b9080f751c0b90
hoanln <hoanln@deha-soft.com>
2022-11-22 11:24:49 +0700

    Folder structure

---
+++ src/features/Todo/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoFeature.propTypes = {
+  
+};
+
+function TodoFeature(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default TodoFeature;
