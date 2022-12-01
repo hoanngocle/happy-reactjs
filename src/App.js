b638ed38f1c908f3374d3d2267bc0824f3fbec2c
hoanln <hoanngocle.93@gmail.com>
2022-12-01 11:16:17 +0700

    Nested routing

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import './App.css';
-import ColorBox from './components/ColorBox';
-import Counter from './components/ColorBox';
 import GirlFeature from './features/Girl';
 import TodoFeature from './features/Todo';
 
 function App() {
-  const name = 'Emilia';
-  const age = 28;
-  const isMale = true;
-  const student = {
-    name: 'Hayd'
-  };
-
-  const colorList = ['red', 'blue', 'green'];
-
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {name}!
-        </p>
-        <p>Xin chao {name} - {age} - {isMale ? 'Male': 'Female'}</p>
-        {isMale ? <p>Male</p> : <p>Female</p>}
-
-        {isMale && (
-          <React.Fragment>
-          <p>Male</p>
-          <p>Male</p>
-          <p>Male</p>
-          </React.Fragment>
-        )}
-
-
-        {isMale && (
-          <>
-          <p>Male</p>
-          <p>Male</p>
-          <p>Male</p>
-          </>
-        )}
-
-        <p>{student.name}      </p>
-
-        <p>{colorList}</p>
-        <ul>
-          {colorList.map(color => (
-            <li style={{ color }}>{color}</li>
-          ))}
-        </ul>
-      </header> */}
       Home Page
       <p>
         <NavLink to={'/todos'} activeClassName="active-menu">
@@ -64,16 +19,13 @@ function App() {
         </NavLink>
       </p>
       <Switch>
-        {/* <Redirect from="/home" to="/" /> */}
         <Redirect from="/home" to="/" />
         <Redirect from="/post-list/:postId" to="/posts/:postId" exact />
 
         <Route path="/" component={TodoFeature} exact />
-        <Route path="/todos" component={TodoFeature} />
+        <Route path="/todo-list" component={TodoFeature} />
         <Route path="/girls" component={GirlFeature} />
       </Switch>
-      {/* <ColorBox/> */}
-      {/* <Counter /> */}
     </div>
   );
 }
---
+++ src/features/Todo/pages/Detail/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+Detail.propTypes = {};
+
+function Detail(props) {
+  return <div>Todo Detail Page</div>;
+}
+
+export default Detail;
