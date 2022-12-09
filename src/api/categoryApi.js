c5c9333681b827620a50d6e3daedb2593aec274f
emilia <hoanngocle.93@gmail.com>
2022-12-09 10:02:08 +0700

    Add category API

---
+++ src/api/categoryApi.js
@@ -0,0 +1,35 @@
+import axiosClient from './axiosClient';
+
+const categoryApi = {
+  getAll(params) {
+    const url = '/categories';
+
+    return axiosClient.get(url, { params });
+  },
+
+  get(id) {
+    const url = `/categories/${id}`;
+
+    return axiosClient.get(url);
+  },
+
+  add(data) {
+    const url = 'categories';
+
+    return axiosClient.post(url, data);
+  },
+
+  update(data) {
+    const url = `/categories/${id}`;
+
+    return axiosClient.patch(url);
+  },
+
+  remove(id) {
+    const url = `/categories/${id}`;
+
+    return axiosClient.delete(url);
+  },
+};
+
+export default categoryApi;
